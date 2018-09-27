c2bc1266652512c3dd3451339830317da497990a
Shu <ds303077135@gmail.com>
2018-09-27 19:36:16 +0800

    Update styles, add art

--- templates/page.js
+++ templates/page.js
@@ -1,14 +1,11 @@
 import { MDXProvider } from '@mdx-js/tag'
-import Head from 'next/head'
 
+import Head from './helpers/head'
 import theme from '../theme'
 
 export default meta => ({ children }) => <MDXProvider components={theme.components}><>
   <Head>
     <title>{meta.title}</title>
-    <meta charSet="utf-8"/>
-    <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
   </Head>
   <theme.Container>
     <theme.components.h1>{meta.title}</theme.components.h1>
--- theme/content.js
+++ theme/content.js
@@ -5,8 +5,29 @@ export default ({ children }) => (
       div {
         margin: 2rem 0 5rem;
       }
+      div :global(p) {
+        margin-top: 1.5rem;
+        margin-bottom: 0;
+      }
+      div :global(img) {
+        max-width: 100%;
+      }
       // :global(:target) {
       //   background: #fffa9e;
+      // }
+      // :global(ul) {
+      //   list-style-type: none;
+      //   padding: 0;
+      // }
+      // :global(li) {
+      //   padding-left: 1.1875em;
+      //   position: relative;
+      //   margin-bottom: .25em;
+      // }
+      // :global(li):before {
+      //   content: "•";
+      //   position: absolute;
+      //   left: .2em;
       // }
     `}</style>
   </div>
